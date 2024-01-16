9f5d771f912368f5530bc3c5ba72da7ca64dd586
Federico <higafederico@gmail.com>
2024-01-16 17:27:04 -0300

    challenge3_e-commerce2.0

---
+++ assets/js/js.js
@@ -0,0 +1,7 @@
+const loginButton = document.getElementById("login");
+
+const loginPage = ()=>{
+    window.location.href = "http://127.0.0.1:5500/ONE--main/Challenges/Challenge-3-e-commerce/screens/login.html"
+};
+
+loginButton.addEventListener("click", loginPage);
---
+++ controllers/product-POST-controller.js
@@ -0,0 +1,29 @@
+import { productService } from "../service/productos-services.js";
+
+const form = document.querySelector("[data-form]")
+
+/**
+ * 1)listener del formulario para crear producto
+ * 2)captura los .values del form/CRUD q ingresa el usuario 
+ * 3)hace la llamada del productPost() con dichos valores y los guarda en el db.json
+ */
+
+form.addEventListener("submit", (event)=>{
+    
+    event.preventDefault();
+
+    const name = document.getElementById("nombre").value
+    const imgURL = document.getElementById("imgURL").value
+    const price = document.getElementById("price").value
+    const categoria = document.getElementById("categoria").value
+
+    productService.productPost(name, imgURL, price, categoria)
+    .then (() =>{
+        window.location.href= "../index.html"
+        alert("Producto agregado con exito");
+    }).catch (error=>{
+        console.log(error);
+    })
+})
+
+
