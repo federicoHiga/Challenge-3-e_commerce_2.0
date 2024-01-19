b8899a62422944bc172c6f876f2f3ef5a0a7ce5b
Federico <higafederico@gmail.com>
2024-01-19 10:53:26 -0300

    allProductsToArray

--- service/productos-services.js
+++ service/productos-services.js
@@ -3,22 +3,25 @@
 //GET
 
 const productList = ()=>{
+    console.log("antes del get")
    return fetch ("https://federicohiga.github.io/Challenge-3-e_commerce_2.0/db.json")
     .then ((response)=> response.json());
 }
+console.log("despoes del get")
 
 
 //POST
 
 const productPost = (name, imgURL, price, categoria)=>{
+    console.log("antes del fetch")
     return fetch ("https://federicohiga.github.io/Challenge-3-e_commerce_2.0/db.json", {
         method: "POST",
         headers: {
             "Content-type": "application/json"
-        },
+        }, 
         body: JSON.stringify({name, imgURL, price, categoria, id: uuid.v4()},)
     }).then(response =>{
-        console.log(response.body)
+        console.log("despues del fetch",response.body)
         if(response.ok){
             return response.body
         }
